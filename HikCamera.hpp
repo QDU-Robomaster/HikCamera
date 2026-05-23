2bf75fd94ad7e6f2b5b27635ff07acd529af26e3
Xiao <2592509183@qq.com>
2026-05-23 14:13:39 +0100

    feat(camera): halve hik capture output in software

--- HikCamera.hpp
+++ HikCamera.hpp
@@ -800,6 +800,7 @@ class HikCamera : public LibXR::Application,
   void CaptureLoop()
   {
     WaitForImageSink();
+    uint32_t software_frame_div_counter = 0;
     while (camera_state_.load())
     {
       ImageFrame* image = this->GetWritableImage();
@@ -832,6 +833,12 @@ class HikCamera : public LibXR::Application,
       if (!ResolveImageTimestampUs(frame_info, image_timestamp_us))
       {
         ++failure_count_;
+        continue;
+      }
+
+      ++software_frame_div_counter;
+      if ((software_frame_div_counter & 1U) != 0U)
+      {
         continue;
       }
 
